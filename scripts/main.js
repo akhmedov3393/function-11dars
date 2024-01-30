88c210dc90049ca56777492ba4feae2c57e6b983
akhmedov3393 <akhmedov3393@gmail.com>
2024-01-30 16:01:01 +0500

    yoshga qarab tartiblash

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+
+
+// function factorial(num) {
+//  if(num===0 || num===1)  return 1;
+//  return num * factorial(num - 1);
+// }
+
+// .
+
+// console.log(factorial(5))
+
+// const humans = [
+//   { id: 1, age: 5, name: "sakjsd" },
+//   { id: 2, age: 15, name: "asd" },
+//   { id: 3, age: 25, name: "dsa" },
+//   { id: 4, age: 35, name: "asad" },
+//   { id: 5, age: 15, name: "asddsa" },
+//   { id: 6, age: 5, name: "akmal" },
+//   { id: 7, age: 7, name: "javohir" },
+//   { id: 8, age: 12, name: "java" },
+//   { id: 9, age: 45, name: "css" },
+//   { id: 10, age: 78, name: "c++" },
+//   { id: 11, age: 23, name: "js" },
+//   { id: 12, age: 11, name: "python" },
+//   { id: 13, age: 16, name: "adham" },
+//   { id: 14, age: 23, name: "sarvar" },
+//   { id: 15, age: 24, name: "ruxshon" },
+//   { id: 16, age: 42, name: "sanjar" },
+//   { id: 17, age: 4, name: "abror" },
+//   { id: 18, age: 5, name: "asror" },
+//   { id: 19, age: 25, name: "axror" },
+//   { id: 20, age: 35, name: "ilhom" },
+//   { id: 21, age: 36, name: "jasur" },
+//   { id: 22, age: 56, name: "akbar" },
+//   { id: 23, age: 45, name: "sherzod" },
+//   { id: 24, age: 7, name: "sardor" },
+//   { id: 25, age: 21, name: "odil" },
+//   { id: 26, age: 14, name: "iskandar" },
+//   { id: 27, age: 13, name: "shalola" },
+//   { id: 28, age: 28, name: "shaxzoda" },
+//   { id: 29, age: 3, name: "umida" },
+// ];
+
+// const bogchadagilar = [];
+// const maktabdagilar = [];
+// const unversitetdagilar = [];
+// const ishchilar = [];
+
+// const filterByAge = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].age < 7) {
+//       bogchadagilar.push(array[i].name);
+//     } else if (array[i].age>=7&&array[i].age<=18){
+//         maktabdagilar.push(array[i].name)
+//     } else if ( array[i].age>18&& array[i].age<=24){
+//         unversitetdagilar.push(array[i].name)
+//     } else if(array[i].age>=25){
+//         ishchilar.push(array[i].name)
+//     }
+//   }
+// };
+// console.log('bogchaga boradiganlar:',bogchadagilar);
+// console.log('maktabga boradiganlar:',maktabdagilar);
+// console.log('universitetga boradiganlar:',unversitetdagilar);
+// console.log('ishlidganlar boradiganlar:',ishchilar);
+
+// filterByAge(humans);
